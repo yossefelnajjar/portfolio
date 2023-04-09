756b86cb8343a0651754261f461408210b924b61
yossef elnajjar <yossefelnajjar@gmail.com>
2023-04-09 07:35:46 +0200

    Adding Cv Button

--- src/components/CvBtn.js
+++ src/components/CvBtn.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CvBtn = () => {
   return (
-    <div className="fixed top-0 right-0 p-3 text-2xl  z-50">
+    <div className="fixed top-0 right-0 p-3 text-2xl z-50 underline">
       <a href="../assets/CV.pdf" download>
         CV
         <FontAwesomeIcon icon={faFileLines} className="ml-1" />
